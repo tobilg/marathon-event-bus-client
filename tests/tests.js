3dafcaaa3e6378b74213b7ef70b6e8978ec25e2f
tobilg <tobilg@gmail.com>
2017-01-06 13:41:58 +0100

    Externalized TestSSEServer

--- tests/tests.js
+++ tests/tests.js
@@ -12,79 +12,7 @@ const sinon = require("sinon");
 
 const MarathonEventBusClient = require("../index");
 const exampleEvents = require("./exampleEvents");
-
-/**
- * TestSSEServer test server.
- */
-class TestSSEServer {
-
-    constructor(port) {
-        this.port = port;
-        this.connectionCache = {};
-        this.server = http.createServer();
-
-        this.app = express();
-
-        this.server = this.app.listen(this.port, function (err) {
-            if (err) throw err;
-            console.log("SSE server ready on http://localhost:" + port)
-        });
-
-        this.sse = new SSE(this.server, { path: "/v2/events" });
-
-        let self = this;
-
-        //
-        this.sse.on("connection", function (connection) {
-            let connectionId = crypto.randomBytes(16).toString("hex");
-
-            // Store connection in connectionCache
-            self.connectionCache[connectionId] = connection;
-
-            self.connectionCache[connectionId].send({
-                event: "connectionId",
-                data: JSON.stringify(connectionId)
-            });
-
-            console.log("new connection with id "+ connectionId);
-
-            self.connectionCache[connectionId].on("close", function () {
-                console.log("Closed connection " + connectionId);
-            });
-        });
-
-    }
-
-    listen() {
-        console.log("Listening for SSE requests on port " + this.port);
-        this.server.listen(this.port);
-    }
-
-    close() {
-        this.server.close();
-    }
-
-    closeConnection(connectionId) {
-        if (this.connectionCache.hasOwnProperty(connectionId)) {
-            console.log("Closing connectionId " + connectionId);
-            this.connectionCache[connectionId].close();
-        } else {
-            console.log("Connection with connectionId " + connectionId + " doesn't exist!");
-        }
-    }
-
-    requestEvent(eventType) {
-        var self = this;
-        if (exampleEvents.hasOwnProperty(eventType)) {
-            Object.getOwnPropertyNames(this.connectionCache).forEach(function (connectionId) {
-                self.connectionCache[connectionId].send({
-                    event: eventType,
-                    data: JSON.stringify(exampleEvents[eventType])
-                });
-            })
-        }
-    }
-}
+const TestSSEServer = require("./TestSSEServer");
 
 describe("MarathonEventBusClient tests", function() {
 
@@ -119,11 +47,11 @@ describe("MarathonEventBusClient tests", function() {
             mebc.subscribe();
 
             mebc.on("connected", function () {
-                mebc.logger.info("connected!");
+                console.log("connected!");
             });
 
             mebc.on("connectionId", function (connectionId) {
-                mebc.logger.info("Using connectionId " + connectionId);
+                console.log("Using connectionId " + connectionId);
 
                 const EVENT_TYPE = "deployment_info";
 
